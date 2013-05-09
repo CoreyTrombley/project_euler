694b453b9f13c8090f9a1ae0094a82f16a57f2ab
Corey Trombley <corey.trombley33@gmail.com>
2013-05-09 15:24:27 -0400

    refactored number 4

--- project_euler_4.rb
+++ project_euler_4.rb
@@ -6,27 +6,17 @@
 # of two 3-digit numbers.
 
 def pal
-  i = 0
   f = (100..999).to_a
   a = []
-  (100..899).each do |i|
-    (100..899).each do |j|
-      puts "the first number is #{f[i]}"
-      puts "the second number is #{f[j]}"
+  (1..899).each do |i|
+    (1..899).each do |j|
       num = f[i] * f[j]
-      puts num
       if num == num.to_s.reverse.to_i
-        puts "============================"
-        puts num
-        puts "============================"
         a << num
-      else
-        puts "nope"
-        puts "===="
       end
       j += 1
     end
     i += 1 
   end
-  return a.sort
+  return a.sort.last
 end
